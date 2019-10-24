6fb713586faad288aa3dbb82db4b23dee98a0622
Kiet <kietpham99.kp@gmail.com>
2019-10-24 15:44:04 -0700

    Moved UCSD email check to separate file

---
+++ server/utitlities/ucsdMailCheck.js
@@ -0,0 +1,13 @@
+module.exports = {
+    checkMail: (email) => {
+        const allowed_domain = "ucsd.edu";
+        // Check to make sure domain is UCSD account
+        // negative important in slice to get last characters
+        if (email.slice(-allowed_domain.length) != allowed_domain) {
+            console.log("NOT UCSD ACCOUNT!")
+            return false;
+        }
+
+        return true;
+    },
+};
